61e6fdf6dfa0316b852c4d73f851b8b7388ad32e
ankur-roy-byte <ankurroy1729@gmail.com>
2021-02-21 11:17:34 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'eventFrontend';
+  title = 'My Events';
+  
+constructor(){
+  this.myfunction(25,5);
+}
+  myfunction(data,arg2){
+      let sum = data+arg2;
+      let subs = data- arg2;
+      let mul = data*arg2;
+      let div = data/arg2;
+      let reminder = data % arg2;
+
+      console.log("my sum is",sum);
+  }
+  // ngOndestroy(){
+
+  // }
 }
